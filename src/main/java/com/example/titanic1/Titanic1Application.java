f7e92e3da078765f089d68a3169ba8aa4192a0b2
synteticdiva <alina.sheveleva2023@mail.ru>
2024-07-25 18:11:36 +0300

    add main

---
+++ src/main/java/com/example/titanic1/Titanic1Application.java
@@ -0,0 +1,16 @@
+package com.example.titanic1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableCaching
+public class Titanic1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Titanic1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/titanic1/Titanic1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.titanic1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Titanic1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
